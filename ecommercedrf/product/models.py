abf9165e3cf7515434f7330816d8629fa9831980
m-arafa2 <mahamedarafa@ymail.com>
2023-03-04 07:08:44 +0200

    added models

--- ecommercedrf/product/models.py
+++ ecommercedrf/product/models.py
@@ -1,12 +1,28 @@
 from django.db import models
-
 # Create your models here.
+from mptt.models import MPTTModel,TreeForeignKey
+class Category(MPTTModel):
+    name = models.CharField(max_length=100)
+    parent =TreeForeignKey("self", on_delete=models.PROTECT,null=True,blank=True)
+    class MPTTMeta:
+        order_insertion_by =["name"]
+        
+    def __str__(self):
+        return self.name
+    
 class Brand(models.Model):
     brand = models.CharField(max_length=100)
     
+    def __str__(self):
+        return self.name
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    is_digital = models.BooleanField(default=False)
+    Category = TreeForeignKey("Category",on_delete=models.PROTECT,null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
     
 
